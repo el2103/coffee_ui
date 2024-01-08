4a04cf784dcb713051258d76e0b2af8fcd85960c
el2103 <bellari0311@gmail.com>
2024-01-08 20:47:42 +0800

    first

---
+++ lib/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Color colorTheme = Color(0xff023E4A);
+Color greyColor = Color(0xff687A79);
+
+// textstyle
+TextStyle boldText = TextStyle(color: colorTheme, fontWeight: FontWeight.w700);
+TextStyle regularText = TextStyle(color: greyColor, fontWeight: FontWeight.w500);
+TextStyle semiboldText = TextStyle(color: greyColor, fontWeight: FontWeight.w600);
